392e151112a6a6fe03d3d10bb85797310c6190c2
unknown <zhiminzou@gmail.com>
2020-09-05 17:02:49 -0500

    Added hall sensor RPM RPS counter

--- Wireless Com/Reciever/Reciever.ino
+++ Wireless Com/Reciever/Reciever.ino
@@ -12,6 +12,7 @@
 #include <RF24.h>
 RF24 radio(7, 8); // CE, CSN
 const byte address[6] = "00001";
+#define PI 3.1415926535897932384626433832795
 
 //Hall Motor Control
 int pwm_pin = 2;
@@ -21,12 +22,15 @@ long currentPWM = 0; //current PWM duty cycle
 //ENCODER
 //declaring the variables used for the encoder
 int hallOn = false;
-int encoder = 2;
 float rps;
 float rpm;
+float mph;
 volatile byte pulses;
 unsigned long timeold;
-float s;
+float wheelSize = 70; //Diameter of wheel for Speed Calc
+
+
+int halltest = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -43,7 +47,6 @@ void setup() {
     rps=0;
     rpm=0;
     timeold=0;
-    s=0;    
 }
 
 void loop() {
@@ -66,20 +69,24 @@ void loop() {
   hallOn = digitalRead(hall_pin);  // read input value
   //Serial.println(hallOn);
 
-  if (pulses>=1) { //this part of the code calculates the rpm and rps in a way that every time the magnet passes by the sensor, the time between the pulses is measured and rpm and rps is calculated
-    rpm = 60000.0/(millis()-timeold)*pulses;
+  if ((millis()-timeold) > 100) { //this part of the code calculates the rpm and rps in a way that every time the magnet passes by the sensor, the time between the pulses is measured and rpm and rps is calculated
+    rpm = 60000.0/(millis()-timeold)*pulses / 10; //10 rising edges per revolution
     rps=rpm/60.0;
+    mph=rps*60*60*PI*wheelSize*1e-6*0.621371;
     timeold = millis();
     pulses=0;
   }
-  Serial.println(rps);
+  if (rps > 0) {
+    //Serial.println(rps);
+    Serial.println(mph);
+  }
   rps = 0;
 } //loop ends
 
 //this subprogram will run when interrupt is triggered
 void counter()
 {
-  pulses++; //every time this subprogram runs, the number of pulses is increased by 1 
-  s = s + 20.42; //and the variable s is increased by 20.42cm, which is the circumference of my wheel, s stands for path
-  
+  pulses++; //every time this subprogram runs, the number of pulses is increased by 1
+  halltest++;
+  //Serial.println(halltest);
 }
